9de07e276ec0d1fbdd82031100c72055027df9b4
Jeral Pineda <jeral.cris@gmail.com>
2024-10-22 21:39:21 -0600

    fix: mapear respuestas a objetos personalizados

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,14 +1,12 @@
 import { Text, View } from "react-native";
+
 import "../global.css";
-import { nowPlayingAction } from "@/core/actions/movies/now-playing.action";
 
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  nowPlayingAction();
-
   return (
     <View className="flex-1 items-center justify-center">
       <Text>Hello World</Text>
--- core/actions/movies/now-playing.action.ts
+++ core/actions/movies/now-playing.action.ts
@@ -1,16 +1,12 @@
 import { movieApi } from "@/core/api/movie-api";
 import { MovieResponse } from "@/infrastructure/interfaces/movie-response";
+import { MovieMapper } from "@/infrastructure/mappers/movie-mapper";
 
 export const nowPlayingAction = async () => {
   try {
     const { data } = await movieApi.get<MovieResponse>("/now_playing");
-
-    console.log(
-      "🚀 now-playing.action.ts -> #7 ~",
-      JSON.stringify(data, null, 2),
-    );
-
-    return [];
+    const movies = data.results.map(MovieMapper.fromTheMovieDBToMovie);
+    return movies;
   } catch (error) {
     console.log(error);
     throw "Cannot load now playing movies";
