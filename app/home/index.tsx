f852ff9a638f091adc8f5e2d9d38cca33087de51
Jeral Pineda <jeral.cris@gmail.com>
2024-10-23 20:49:31 -0600

    fix: Pantalla de carga y SafeArea manual

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -1,12 +1,22 @@
-import { Text, View } from "react-native";
+import { ActivityIndicator, Text, View } from "react-native";
+import { useSafeAreaInsets } from "react-native-safe-area-context";
 import { useMovies } from "@/presentation/hooks/useMovies";
 
 export default function Home() {
+  const safeArea = useSafeAreaInsets();
   const { nowPlayingQuery } = useMovies();
 
+  if (nowPlayingQuery.isLoading) {
+    return (
+      <View className="flex-1 items-center justify-center">
+        <ActivityIndicator color="purple" size={40} />
+      </View>
+    );
+  }
+
   return (
-    <View>
-      <Text>{JSON.stringify(nowPlayingQuery.data)}</Text>
+    <View className="mt-2" style={{ paddingTop: safeArea.top }}>
+      <Text className="text-3xl font-bold px-4 mb-2">Home</Text>
     </View>
   );
 }
