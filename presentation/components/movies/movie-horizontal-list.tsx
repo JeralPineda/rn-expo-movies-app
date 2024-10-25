7eb2e27707ce49a3bd8326dd03ee00c4bf686d45
Jeral Pineda <jeral.cris@gmail.com>
2024-10-24 20:08:51 -0600

    fix: determinar fin de scroll de un FlatList

--- presentation/components/movies/movie-horizontal-list.tsx
+++ presentation/components/movies/movie-horizontal-list.tsx
@@ -1,19 +1,49 @@
 import { Movie } from "@/infrastructure/interfaces/movie.interface";
-import { FlatList, Text, View } from "react-native";
+import {
+  FlatList,
+  NativeScrollEvent,
+  NativeSyntheticEvent,
+  Text,
+  View,
+} from "react-native";
 
 import MoviePoster from "./movie-poster";
+import { useRef } from "react";
 
 interface MovieHorizontalListProps {
   title: string;
   movies: Movie[];
   className?: string;
+
+  loadNextPage?: () => void;
 }
 
 export default function MovieHorizontalList({
   title,
   movies,
   className,
+  loadNextPage,
 }: MovieHorizontalListProps) {
+  const isLoading = useRef(false);
+
+  const onScroll = (event: NativeSyntheticEvent<NativeScrollEvent>) => {
+    if (isLoading.current) return;
+
+    const { contentOffset, layoutMeasurement, contentSize } = event.nativeEvent;
+
+    // Determinar si se ha llegado al final de la lista o esta cerca
+    const isEndReached =
+      contentOffset.x + layoutMeasurement.width + 600 >= contentSize.width;
+
+    if (!isEndReached) return;
+
+    isLoading.current = true;
+
+    // Cargar más películas
+    console.log("Cargando más películas");
+    loadNextPage && loadNextPage();
+  };
+
   return (
     <View className={`${className}`}>
       {title && <Text className="text-2xl font-bold px-4 mb-2">{title}</Text>}
@@ -24,6 +54,7 @@ export default function MovieHorizontalList({
         renderItem={({ item }) => (
           <MoviePoster id={item.id} poster={item.poster} smallPoster />
         )}
+        onScroll={onScroll}
         showsHorizontalScrollIndicator={false}
         horizontal
       />
